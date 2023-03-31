ba5b9e466e45753be0a816e784012b44d86c0897
jimmy-zx <51149050+jimmy-zx@users.noreply.github.com>
2023-03-31 17:10:18 -0400

    Updated asm examples

--- examples/asm/inc.s
+++ examples/asm/inc.s
@@ -1,4 +1,4 @@
-.macro	fprologue
+.macro	fprol
 	addiu	$sp,$sp,-24	# allocate a minimal stack (16+2*4)
 	sw	$ra,20($sp)
 	sw	$fp,16($sp)
@@ -9,11 +9,37 @@
 	sw	$a0,24($sp)
 .endm
 
-.macro	fepilogue
+.macro	fepil
 	move	$sp,$fp
 	lw	$ra,20($sp)
 	lw	$fp,16($sp)
 	addiu	$sp,$sp,24
+.endm
+
+.macro	savesr
+	addiu	$sp,$sp,32	# allocate stack for $s* (8*4)
+	move	$fp,$sp
+	sw	$s7,44($sp)
+	sw	$s6,40($sp)
+	sw	$s5,36($sp)
+	sw	$s4,32($sp)
+	sw	$s3,28($sp)
+	sw	$s2,24($sp)
+	sw	$s1,20($sp)
+	sw	$s0,16($sp)
+.endm
+
+.macro	loadsr
+	lw	$s7,44($sp)
+	lw	$s6,40($sp)
+	lw	$s5,36($sp)
+	lw	$s4,32($sp)
+	lw	$s3,28($sp)
+	lw	$s2,24($sp)
+	lw	$s1,20($sp)
+	lw	$s0,16($sp)
+	addiu	$sp,$sp,-32
+	move	$fp,$sp
 .endm
 
 
